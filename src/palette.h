3217cca9365014a6a75bd150ff1e14100950700b
Alberth289346 <Alberth289346@gmail.com>
2011-09-18 19:07:11 +0000

    -Add: Add 8bpp palette.

---
+++ src/palette.h
@@ -0,0 +1,27 @@
+/* $Id$ */
+
+/*
+ * This file is part of FreeRCT.
+ * FreeRCT is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+ * FreeRCT is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with FreeRCT. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file palette.h 8bpp palette definitions. */
+
+#ifndef PALETTE_H
+#define PALETTE_H
+
+/** Colours. */
+enum PaletteColours {
+	COL_BACKGROUND = 0,     ///< Index of background behind the world display.
+
+	COL_SERIES_START = 10,  ///< Index of the first series.
+	COL_SERIES_LENGTH = 12, ///< Number of shades in a single run.
+	COL_NUMBER_SERIES = 18, ///< Number of series.
+	COL_SERIES_END = COL_SERIES_START + COL_NUMBER_SERIES * COL_SERIES_LENGTH, ///< First colour after the series.
+};
+
+extern const uint8 _palette[256][3]; ///< The 8bpp FreeRCT palette.
+
+#endif
