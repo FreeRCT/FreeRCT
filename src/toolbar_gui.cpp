53dcf57dbccaf68f9a4296dc0c9c188751f8eb66
Charles Pigott <LordAro@users.noreply.github.com>
2013-10-14 20:55:05 +0000

    -Codechange: Extra file for the bottom toolbar isn't necessary.

--- src/toolbar_gui.cpp
+++ src/toolbar_gui.cpp
@@ -14,11 +14,14 @@
 #include "window.h"
 #include "video.h"
 #include "language.h"
+#include "finances.h"
+#include "dates.h"
+#include "math_func.h"
 
 void ShowQuitProgram();
 
 /**
- * Main toolbar.
+ * Top toolbar.
  * @ingroup gui_group
  */
 class ToolbarWindow : public GuiWindow {
@@ -60,7 +63,6 @@ static const WidgetPart _toolbar_widgets[] = {
 		Widget(WT_TEXT_PUSHBUTTON, TB_GUI_FINANCES,    COL_RANGE_BROWN), SetData(GUI_TOOLBAR_GUI_FINANCES,    GUI_TOOLBAR_GUI_TOOLTIP_FINANCES),
 	EndContainer(),
 };
-
 
 ToolbarWindow::ToolbarWindow() : GuiWindow(WC_TOOLBAR, ALL_WINDOWS_OF_TYPE)
 {
@@ -117,6 +119,126 @@ void ToolbarWindow::OnClick(WidgetNumber number)
 void ShowToolbar()
 {
 	new ToolbarWindow();
+}
+
+/**
+ * Bottom toolbar.
+ * @ingroup gui_group
+ */
+class BottomToolbarWindow : public GuiWindow {
+public:
+	BottomToolbarWindow();
+
+	Point32 OnInitialPosition() override;
+	void SetWidgetStringParameters(WidgetNumber wid_num) const override;
+	void OnChange(ChangeCode code, uint32 parameter) override;
+	void UpdateWidgetSize(WidgetNumber wid_num, BaseWidget *wid) override;
+};
+
+/**
+ * Widget numbers of the bottom toolbar GUI.
+ * @ingroup gui_group
+ */
+enum BottomToolbarGuiWidgets {
+	BTB_STATUS,  ///< Status panel containing cash and rating readout.
+	BTB_SPACING, ///< Status panel containing nothing (yet).
+	BTB_DATE,    ///< Status panel containing date.
+};
+
+static const uint32 BOTTOM_BAR_HEIGHT = 35;     ///< Minimum Y-coord size of the bottom toolbar (BTB) panel.
+static const uint32 BOTTOM_BAR_POSITION_X = 75; ///< Separation of the toolbar from the edge of the window.
+
+/**
+ * Widget parts of the bottom toolbar GUI.
+ * @ingroup gui_group
+ * @todo Left/Right Padding get ignored when drawing text widgets
+ */
+static const WidgetPart _bottom_toolbar_widgets[] = {
+	Intermediate(0, 1),
+		Widget(WT_PANEL, INVALID_WIDGET_INDEX, COL_RANGE_BROWN),
+			Intermediate(1, 0), SetPadding(0, 3, 0, 3),
+				Widget(WT_LEFT_TEXT, BTB_STATUS, COL_RANGE_BROWN),
+					SetMinimalSize(1, BOTTOM_BAR_HEIGHT), // Temp X value
+					SetPadding(3, 0, 30, 0),
+					SetData(STR_ARG1, STR_NULL),
+				Widget(WT_EMPTY, BTB_SPACING, COL_RANGE_BROWN),
+					SetMinimalSize(1, BOTTOM_BAR_HEIGHT), // Temp X value
+				Widget(WT_RIGHT_TEXT, BTB_DATE, COL_RANGE_BROWN),
+					SetMinimalSize(1, BOTTOM_BAR_HEIGHT), // Temp X value
+					SetPadding(3, 0, 30, 0),
+					SetData(STR_ARG1, STR_NULL),
+			EndContainer(),
+	EndContainer(),
+};
+
+BottomToolbarWindow::BottomToolbarWindow() : GuiWindow(WC_BOTTOM_TOOLBAR, ALL_WINDOWS_OF_TYPE)
+{
+	this->SetupWidgetTree(_bottom_toolbar_widgets, lengthof(_bottom_toolbar_widgets));
+}
+
+Point32 BottomToolbarWindow::OnInitialPosition()
+{
+	static Point32 pt;
+	pt.x = BOTTOM_BAR_POSITION_X;
+	pt.y = _video->GetYSize() - BOTTOM_BAR_HEIGHT;
+	return pt;
+}
+
+void BottomToolbarWindow::SetWidgetStringParameters(WidgetNumber wid_num) const
+{
+	switch (wid_num) {
+		case BTB_STATUS:
+			_finances_manager.CashToStrParams();
+			break;
+
+		case BTB_DATE:
+			_str_params.SetDate(1, _date);
+			break;
+	}
+}
+
+void BottomToolbarWindow::OnChange(ChangeCode code, uint32 parameter)
+{
+	if (code == CHG_DISPLAY_OLD) this->MarkDirty();
+}
+
+void BottomToolbarWindow::UpdateWidgetSize(WidgetNumber wid_num, BaseWidget *wid)
+{
+	/* -99,999,999.99 = Maximum amount of money that is worth handling for now. */
+	static const int64 LARGE_MONEY_AMOUNT = -9999999999;
+	Point32 p;
+	p.x = 0;
+	p.y = 0;
+
+	switch (wid_num) {
+		case BTB_STATUS:
+			p = GetMoneyStringSize(LARGE_MONEY_AMOUNT);
+			break;
+
+		case BTB_SPACING: {
+			Point32 money = GetMoneyStringSize(LARGE_MONEY_AMOUNT);
+			Point32 date = GetMaxDateSize();
+			p.x = _video->GetXSize() - (2 * BOTTOM_BAR_POSITION_X) - money.x - date.x;
+			p.y = BOTTOM_BAR_HEIGHT;
+			break;
+		}
+
+		case BTB_DATE:
+			p = GetMaxDateSize();
+			break;
+	}
+
+	wid->min_x = max(wid->min_x, (uint16)p.x);
+	wid->min_y = max(wid->min_y, (uint16)p.y);
+}
+
+/**
+ * Open the bottom toolbar window.
+ * @ingroup gui_group
+ */
+void ShowBottomToolbar()
+{
+	new BottomToolbarWindow();
 }
 
 /** %Window to ask confirmation for ending the program. */
