f170409f52f37af5977a7f798de1060f640d5197
sgtbigman <sgtbigman@gmail.com>
2019-06-19 21:38:35 -0400

    Update text_gui doxygen comment.

--- src/text_gui.cpp
+++ src/text_gui.cpp
@@ -5,7 +5,7 @@
  * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with FreeRCT. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/** @file textbox.cpp Implementation of the textbox widget. */
+/** @file text_gui.cpp %Window for manipulating text. */
 
 #include "stdafx.h"
 #include "window.h"
