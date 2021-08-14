29d9705edbafa9383ca735d27f88243574840be6
Benedikt Straub <benedikt-straub@web.de>
2021-08-14 13:19:19 +0200

    More tweaks

--- docs/slideshow.js
+++ docs/slideshow.js
@@ -16,17 +16,17 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var dots = document.getElementsByClassName("slideshow_dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" slideshow_dot_active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " slideshow_dot_active";
 }
 
 function showSlidesAuto() {
