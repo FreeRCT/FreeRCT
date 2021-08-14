b2a0068125bc8a77e3afecd26c6019509ebbd13f
Benedikt Straub <benedikt-straub@web.de>
2021-08-14 16:23:43 +0200

    now with a nice dropdown

--- docs/menubar.js
+++ docs/menubar.js
@@ -1,11 +1,35 @@
 const ALL_PAGES = [
-	/* Syntax: document name (minus the .html), Label in the menu bar */
-	['index'      , 'FreeRCT Home'],
-	['download'   , 'Download'    ],
-	['manual'     , 'Manual'      ],
-	['screenshots', 'Screenshots' ],
-	['development', 'Development' ]
+	/* Syntax: document name (minus the .html); Label in the menu bar; Open in new tab; Dropdown entries in the same format (or ``null`` if it's not a dropdown). */
+	['index'      , 'FreeRCT Home', false, null],
+	['download'   , 'Download'    , false, null],
+	['manual'     , 'Manual'      , false, null],
+	['screenshots', 'Screenshots' , false, null],
+	['development', 'Development' , false, [
+		['https://github.com/FreeRCT/FreeRCT'       , 'Git Repository', true],
+		['https://github.com/FreeRCT/FreeRCT/issues', 'Issue Tracker' , true],
+	]]
 ];
+
+function makeHref(id) {
+	const isAbsolute = id[0].startsWith('http');
+	var hrefTag = '<a href="' + id[0]
+	if (!isAbsolute) hrefTag += '.html';
+	hrefTag += '"';
+
+	if (!isAbsolute &&
+		(document.URL.search('/docs/' + id[0] + '.html') >= 0 ||
+			document.URL.search('/FreeRCT/' + id[0] + '.html') >= 0 ||
+			(id[0] == 'index' && document.URL.endsWith('FreeRCT/')))) {
+		hrefTag += ' class="menubar_active"';
+	}
+
+	if (id[2]) {
+		hrefTag += ' target="_blank"';
+	}
+
+	hrefTag += '>' + id[1] + '</a>';
+	return hrefTag;
+}
 
 document.write('<link rel="icon" href="images/logo.png">');
 document.write('<ul>');
@@ -17,14 +41,18 @@ document.write('<ul>');
 	document.write('</li>');
 
 	ALL_PAGES.forEach(function(id) {
-		document.write('<li>');
-			document.write('<a href="' + id[0] + '.html"');
-			if (document.URL.search('/docs/' + id[0] + '.html') >= 0 ||
-					document.URL.search('/FreeRCT/' + id[0] + '.html') >= 0 ||
-					(id[0] == 'index' && document.URL.endsWith('FreeRCT/'))) {
-				document.write(' class="menubar_active"');
-			}
-			document.write('>' + id[1] + '</a>');
+		if (id[3] == null) {
+			document.write('<li>');
+			document.write(makeHref(id));
+		} else {
+			document.write('<li class="dropdown">');
+			document.write(makeHref(id));
+			document.write('<div class="dropdown-content">');
+				id[3].forEach(function(entry) {
+					document.write(makeHref(entry));
+				});
+			document.write('</div>');
+		}
 		document.write('</li>');
 	});
 document.write('</ul>');
