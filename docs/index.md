9fc125ee3a28c7561ebdff434e60642667d3fd14
Benedikt Straub <benedikt-straub@web.de>
2021-08-14 11:40:45 +0200

    Use HTML

--- docs/index.md
+++
@@ -1,3 +0,0 @@
-# FreeRCT
-
-Welcome to the FreeRCT homepage! This site is still under development.
---
+++ docs/slideshow.js
@@ -0,0 +1,35 @@
+// Based heavily on code from https://www.w3schools.com/howto/howto_js_slideshow.asp
+
+var slideIndex = 0;
+showSlidesAuto();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function showSlidesAuto() {
+  plusSlides(1);
+  setTimeout(showSlidesAuto, 5000);
+} 
