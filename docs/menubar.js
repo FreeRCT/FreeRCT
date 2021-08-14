ae850c12af33e3de986b4511c241453c94356fe4
Benedikt Straub <benedikt-straub@web.de>
2021-08-14 15:12:39 +0200

    Re-add icon and indent better

--- docs/menubar.js
+++ docs/menubar.js
@@ -7,15 +7,24 @@ const ALL_PAGES = [
 	['development', 'Development' ]
 ];
 
-document.write('<ul> <link rel="stylesheet" href="menubar.css"> <li style="width:auto"><a class="pictorial_link" href=index.html><img src="images/logo.png" height="256" width="auto"></img></a></li>');
-ALL_PAGES.forEach(function(id) {
-	document.write('<li><a href="' + id[0] + '.html"');
-	if (document.URL.search('/docs/' + id[0] + '.html') >= 0 ||
-			document.URL.search('/FreeRCT/' + id[0] + '.html') >= 0 ||
-			(id[0] == 'index' && document.URL.endsWith('FreeRCT/'))) {
-		document.write(' class="menubar_active"');
-	}
-	document.write('>' + id[1] + '</a></li>');
-});
+document.write('<link rel="icon" href="images/logo.png">');
+document.write('<ul>');
+	document.write('<link rel="stylesheet" href="menubar.css">');
+	document.write('<li style="width:auto">');
+		document.write('<a class="pictorial_link" href=index.html>');
+			document.write('<img src="images/logo.png" height="256" width="auto"></img>');
+		document.write('</a>');
+	document.write('</li>');
 
+	ALL_PAGES.forEach(function(id) {
+		document.write('<li>');
+			document.write('<a href="' + id[0] + '.html"');
+			if (document.URL.search('/docs/' + id[0] + '.html') >= 0 ||
+					document.URL.search('/FreeRCT/' + id[0] + '.html') >= 0 ||
+					(id[0] == 'index' && document.URL.endsWith('FreeRCT/'))) {
+				document.write(' class="menubar_active"');
+			}
+			document.write('>' + id[1] + '</a>');
+		document.write('</li>');
+	});
 document.write('</ul>');
