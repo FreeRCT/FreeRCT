8eb3124c31b86434c703418675b2906ed7e19cd7
Benedikt Straub <benedikt-straub@web.de>
2021-08-15 11:33:49 +0200

    Responsive layout

--- docs/menubar.js
+++ docs/menubar.js
@@ -30,21 +30,43 @@ function makeHref(id) {
 	return hrefTag;
 }
 
-const MENU_BAR_MAX_HEIGHT = 256;
 const MENU_BAR_BAR_HEIGHT = 50;
-const MENU_BAR_MAX_Y = (MENU_BAR_MAX_HEIGHT - MENU_BAR_BAR_HEIGHT) / 2;
 function readjustMenuBarY() {
-	const scroll = document.body.scrollTop / 2;
-	const newBarY = Math.min(MENU_BAR_MAX_Y, Math.max(0, scroll));
-	const newLogoH = MENU_BAR_MAX_HEIGHT - newBarY * (MENU_BAR_MAX_HEIGHT - MENU_BAR_BAR_HEIGHT) / MENU_BAR_MAX_Y;
-	const newLogoHalfspace = (MENU_BAR_MAX_HEIGHT - newLogoH) / 2;
+	var totalMenuH = 408, logoMaxH = 256, menuSpacer = 298, bottomSpacer = 280, alwaysCollapse = false;
+	if (window.matchMedia("(max-width: 1110px)").matches) {
+		totalMenuH = 50;
+		alwaysCollapse = true;
+		logoMaxH = 50;
+		menuSpacer = 70;
+		bottomSpacer = 60;
+		if (window.matchMedia("(max-width: 915px)").matches) {
+			// TODO
+		}
+	} else if (window.matchMedia("(max-width: 1440px)").matches) {
+		totalMenuH = 204;
+		logoMaxH = 128;
+		menuSpacer = 149;
+		bottomSpacer = 140;
+	}
+	const menuBarMaxY = alwaysCollapse ? 0 : (totalMenuH - MENU_BAR_BAR_HEIGHT) / 2;
+
+	const scroll = document.body.scrollTop / 1.2;
+	const newBarY = Math.min(menuBarMaxY, Math.max(0, scroll));
+	var logo = document.getElementById('menubar_logo');
+
+	const newLogoH = logoMaxH - (alwaysCollapse ? 0 : ((logoMaxH - MENU_BAR_BAR_HEIGHT) * newBarY / menuBarMaxY));
+	const newLogoHalfspace = (logoMaxH - newLogoH) / 2;
+	// console.log("NOCOM: scroll @ " + scroll + ", barY " + newBarY + ", logo Max H " + logoMaxH + ", newLogoH " + newLogoH + ", newLogoHalfspace " + newLogoHalfspace);
 
 	document.getElementById('menubar_ul').style.top = -newBarY;
-	var logo = document.getElementById('menubar_logo');
-	logo.height = newLogoH;
-	logo.style = 'margin-left:' + newLogoHalfspace + ';margin-right:' + newLogoHalfspace; 
+	document.getElementById('menubar_spacer_menu').style.marginRight = menuSpacer;
+	document.getElementById('menubar_spacer_bottom').style.marginBottom = bottomSpacer;
+	logo.style.height = newLogoH;
+	logo.style.marginLeft = newLogoHalfspace;
+	logo.style.marginRight = newLogoHalfspace;
 }
 document.body.onscroll = readjustMenuBarY;
+document.body.onresize = readjustMenuBarY;
 
 function dropdownMouse(dd, inside) {
 	dd.style = inside ? 'background-color: #003000' : '';
@@ -53,12 +75,12 @@ function dropdownMouse(dd, inside) {
 document.write('<link rel="icon" href="images/logo.png">');
 
 document.write('<a class="pictorial_link" href=index.html>');
-	document.write('<img id="menubar_logo" src="images/logo.png" height="256" width="auto"></img>');
+	document.write('<img id="menubar_logo" src="images/logo.png" height=auto width=auto></img>');
 document.write('</a>');
 
 document.write('<ul id="menubar_ul">');
 	document.write('<link rel="stylesheet" href="menubar.css">');
-	document.write('<p style="margin-right:298px"></p>');
+	document.write('<p id="menubar_spacer_menu"></p>');
 
 	ALL_PAGES.forEach(function(id) {
 		if (id.dropdown == null) {
@@ -76,5 +98,6 @@ document.write('<ul id="menubar_ul">');
 		document.write('</li>');
 	});
 document.write('</ul>');
-document.write('<script>readjustMenuBarY();</script>');
-document.write('<p style="margin-bottom:280px"></p>');
+document.write('<p id="menubar_spacer_bottom"></p>');
+
+document.write('<script>readjustMenuBarY();</script>');  // Need to call this last
