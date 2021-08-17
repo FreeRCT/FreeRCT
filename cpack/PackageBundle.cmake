cb46bcbb3bc95448a2b8a276a2e47f5b58002bb3
Benedikt Straub <benedikt-straub@web.de>
2021-08-17 15:22:23 +0200

    Next try for CPack MacOS

--- cpack/PackageBundle.cmake
+++ cpack/PackageBundle.cmake
@@ -3,6 +3,15 @@ string(TIMESTAMP CURRENT_YEAR "%Y")
 set(CPACK_BUNDLE_NAME "FreeRCT")
 set(CPACK_DMG_FORMAT "UDBZ")
 
-include(BundleUtilities)
-set(BU_CHMOD_BUNDLE_ITEMS TRUE)
-fixup_bundle("${CPACK_OUTPUT_FILE_PREFIX}/${CPACK_PACKAGE_FILE_NAME}" "" "")
+# Delay fixup_bundle() till the install step; this makes sure all executables
+# exists and it can do its job.
+install(
+    CODE
+    "
+        include(BundleUtilities)
+        set(BU_CHMOD_BUNDLE_ITEMS TRUE)
+        FILE(GLOB BUNDLE_FILE_NAME ${CPACK_OUTPUT_FILE_PREFIX}/${CPACK_PACKAGE_FILE_NAME}*)
+        fixup_bundle(\"\${BUNDLE_FILE_NAME}\"  \"\" \"\")
+    "
+    DESTINATION .
+    COMPONENT Runtime)
