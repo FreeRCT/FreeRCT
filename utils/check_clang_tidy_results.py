15b381fa71608d72958c3bcf3fff77457fde6404
Benedikt Straub <benedikt-straub@web.de>
2021-09-05 11:21:28 +0200

    Fix clang-tidy

--- utils/check_clang_tidy_results.py
+++ utils/check_clang_tidy_results.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-"""Checks whether clang-tidy warnings that were previously cleaned have
-regressed."""
+"""Checks whether clang-tidy warnings that were previously cleaned have regressed."""
 
 import re
 import sys
@@ -33,11 +32,6 @@ def main():
     with open(log_file) as checkme:
         contents = checkme.readlines()
         for line in contents:
-            if 'third_party' in line:
-                continue
-            # We're not piloting alpha-level checks
-            if 'clang-analyzer-alpha' in line:
-                continue
             if CHECK_REGEX.match(line):
                 print(line.strip())
                 errors = errors + 1
