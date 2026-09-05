eda30b1ec1baf20771a627e16ed1044e4ed7baef
Benedikt Straub <benedikt-straub@web.de>
2026-09-05 12:51:24 +0200

    Add indonesian id_ID language definition

--- src/language_definitions.h
+++ src/language_definitions.h
@@ -60,11 +60,12 @@ constexpr const LanguageDefinition _all_languages[] = {
 	LanguageDefinition("en_US", 2, &plural_rule_standard, &FONT_LATIN),
 	LanguageDefinition("es_ES", 2, &plural_rule_standard, &FONT_LATIN),
 	LanguageDefinition("fr_FR", 2, &plural_rule_french, &FONT_LATIN),
+	LanguageDefinition("id_ID", 2, &plural_rule_one, &FONT_LATIN),
 	LanguageDefinition("nds_DE", 2, &plural_rule_standard, &FONT_LATIN),
 	LanguageDefinition("nl_NL", 2, &plural_rule_standard, &FONT_LATIN),
 	LanguageDefinition("pt_BR", 2, &plural_rule_standard, &FONT_LATIN),
 	LanguageDefinition("sv_SE", 2, &plural_rule_standard, &FONT_LATIN),
-	/* \todo Our file format and Weblate integration currently require exactly 2 plural forms. Actually Chinese has only one. */
+	/* \todo Our file format and Weblate integration currently require exactly 2 plural forms. Actually Chinese and Indonesian have only one. */
 	LanguageDefinition("zh_Hans", 2, &plural_rule_one, &FONT_CJK),
 	LanguageDefinition("zh_Hant", 2, &plural_rule_one, &FONT_CJK),
 };
